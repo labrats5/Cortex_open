724ea8471a8d6384d7b3e0ceff14365851621d9b
Maxwell Corbin <maxwell.corbin@gmail.com>
2012-09-24 11:30:32 -0400

    simple file server

---
+++ simple-express-static-server/README.txt
@@ -0,0 +1,8 @@
+Simple Express Static File Server
+
+how to start
+>node server
+
+or
+
+>node <pathname>
---
+++ simple-express-static-server/server.js
@@ -0,0 +1,20 @@
+//install express using npm via command "npm install express"
+var express = require("express"),
+    app     = express();
+    
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(8000);
